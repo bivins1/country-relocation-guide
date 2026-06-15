3f5d83674ff197d1937ec0052e01c33132663f21
bivins1 <bigviscious33@gmail.com>
2026-06-15 21:56:43 +0100

    asks user for country name validates fetches from api, error checking

--- search_country.py
+++ search_country.py
@@ -12,21 +12,47 @@ def country_search():
     
     headers={'Authorization': 'Bearer rc_live_b04495913d254556bd142eeda6248825'}
     url=f"https://api.restcountries.com/countries/v5/names.common/{country}"
-    response= requests.get(url, headers=headers, )
-    result=response.json()
-    print(type(result))
-    print(result.keys())
-    # print(result["data"])
-    # print(result["data"]["objects"])
-    # print(type(result["data"]["objects"][0]))
-    print(result["data"]["objects"][0].keys())
-    print(result["data"]["objects"][0]["names"]["common"])
-    print(result["data"]["objects"][0]["population"])
-    print(result["data"]["objects"][0]["capitals"][0]["name"])
-    print(result["data"]["objects"][0]["region"])
-    print(result["data"]["objects"][0]["flag"]["emoji"])
-    print(result["data"]["objects"][0]["timezones"])
-    print(result["data"]["objects"][0]["currencies"][0]["name"])
+
+    try:
+
+        response= requests.get(url, headers=headers )
+        response.raise_for_status()
+        result=response.json()
+
+        print(type(result))
+        # print(result.keys())
+        # print(result["data"])
+        print(type(result["data"]["objects"]))
+        print(type(result["data"]["objects"]))
+        print(result["data"]["objects"])
+
+
+
+        country_object=result["data"]["objects"]
+
+        if not country_object:
+            print("country selected does not exist")
+            return
+
+        print(country_object[0].get("names", {}).get("common", "unknown"))
+        print(country_object[0].get("population", 0))
+        capital=country_object[0].get("capitals", [])
+        print(capital[0].get("name", "unknown") if capital else "country has no capital" )
+        print(country_object[0].get("region", "unknown"))
+        print(country_object[0].get("flag", {}).get("emoji", "no flag"))
+        print(", ".join(country_object[0].get("timezones", [])))
+        currencies = country_object[0].get("currencies", [])
+
+        print(currencies[0].get("name", "unknown") if currencies else "No currency")
+
+    except requests.exceptions.HTTPError as e:
+        print(f"error status code: {e.response.status_code}")
+
+    except requests.exceptions.RequestException as error:
+        print(f"error occured: {type(error)}")
+
+   
+       
 
     
 
