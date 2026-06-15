11db1b2546043e7223f2142b33c61075aa07bdd0
bivins1 <bigviscious33@gmail.com>
2026-06-15 16:13:27 +0100

    api item identified, country  data code working

--- search_country.py
+++ search_country.py
@@ -1,36 +1,48 @@
 import re
 import requests
 
-
-
 pattern=r"[A-Za-z]+([ -'][A-Za-z]+)*"
 
 def country_search():
     country = input("what is the country name: ").strip()
 
-    # if not re.fullmatch(pattern, country):
-    #     print("invalid country name")
-    #     return
+    if not re.fullmatch(pattern, country):
+        print("invalid country name")
+        return
     
-    # params={
-    #     "q":country
-    # }
     headers={'Authorization': 'Bearer rc_live_b04495913d254556bd142eeda6248825'}
-    url="https://api.restcountries.com/countries/v5"
-    response= requests.get(url, headers=headers)
+    url=f"https://api.restcountries.com/countries/v5/names.common/{country}"
+    response= requests.get(url, headers=headers, )
     result=response.json()
-    # print(type(result))
-    # print(type(result["data"]["objects"]))
-    print(type(result["data"]["objects"][0]))
+    print(type(result))
+    print(result.keys())
+    # print(result["data"])
+    # print(result["data"]["objects"])
+    # print(type(result["data"]["objects"][0]))
     print(result["data"]["objects"][0].keys())
-
     print(result["data"]["objects"][0]["names"]["common"])
     print(result["data"]["objects"][0]["population"])
-    print(type(result["data"]["objects"][0]["capitals"]))
+    print(result["data"]["objects"][0]["capitals"][0]["name"])
     print(result["data"]["objects"][0]["region"])
-    print(result["data"]["objects"][0]["flag"])
+    print(result["data"]["objects"][0]["flag"]["emoji"])
     print(result["data"]["objects"][0]["timezones"])
-    print(type(result["data"]["objects"][0]["currencies"]))
+    print(result["data"]["objects"][0]["currencies"][0]["name"])
+
+    
+
+    
+    # print(result)
+    # print(type(result["data"]["objects"]))
+    # print(type(result["data"]["objects"][0]))
+    # print(result["data"]["objects"][0].keys())
+
+    # print(result["data"]["objects"][0]["names"]["common"])
+    # print(result["data"]["objects"][0]["population"])
+    # print(result["data"]["objects"][0]["capitals"][0]["name"])
+    # print(result["data"]["objects"][0]["region"])
+    # print(result["data"]["objects"][0]["flag"]["emoji"])
+    # print(result["data"]["objects"][0]["timezones"])
+    # print(result["data"]["objects"][0]["currencies"][0]["name"])
 
     
 
