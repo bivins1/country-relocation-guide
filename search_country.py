78832e2d1e6ad23cabbaae330d85e6f3c7550862
bivins1 <bigviscious33@gmail.com>
2026-06-17 09:18:48 +0100

    ai intergrated and used to get travel and relocation guide

--- search_country.py
+++ search_country.py
@@ -14,11 +14,11 @@ print(f"key loaded successfully {api_key [:4]}")
 
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-3.5-flash", contents="Explain Nigeria in simple terms for a student."
-)
+# response = client.models.generate_content(
+#     model="gemini-3.5-flash", contents="Explain Nigeria in simple terms for a student."
+# )
 
-print(response.text)
+# print(response.text)
 
 pattern = r"[A-Za-z]+([ -'][A-Za-z]+)*"
 
@@ -32,6 +32,8 @@ def country_search():
 
     headers = {"Authorization": "Bearer rc_live_b04495913d254556bd142eeda6248825"}
     url = f"https://api.restcountries.com/countries/v5/names.common/{country_name}"
+
+    country_data=""
 
     try:
         response = requests.get(
@@ -69,6 +71,18 @@ def country_search():
         else:
             print("No currency")
 
+
+        country_data=f"""
+
+          country:{country.get("names", {}).get("common", "unknown")}
+          population:{country.get("population", 0)}
+          capital:{country.get("capitals", [])[0].get("name", "unknown")}
+          region:{country.get("region", "unknown")}
+          flag:{country.get("flag", {}).get("emoji", "no flag")}
+          timezone:{", ".join(country.get("timezones", []))}
+          currencies:{country.get("currencies", [])[0].get("name", "unknown")}
+    """
+
     except requests.exceptions.HTTPError as e:
         print(f"HTTP error: {e.response.status_code}")
         print(e.response.text)
@@ -77,10 +91,64 @@ def country_search():
         print(f"Request error: {type(error)}")
 
 
-   
+
+    country_data
+
+    prompt1=f"""
+
+    you are a travel and relocation assistant.
+
+    generate a simple travel guide for people looking to have a vacation using the provided country data.
+
+    use this structure when providing your answer:
+    -overview
+    -culture
+    -country landmarks
+    -travel tips
+
+    this is the provided country data:
+    {country_data}
+
+
+"""
+    prompt2=f"""
+
+    you are a travel and relocation assistant.
+
+    generate a simple travel guide for people looking to have a relocate using the provided country data.
+
+    use this structure when providing your answer:
+    -overview
+    -cost of living
+    -culture
+    -security
+    -travel tips
+
+    this is the provided country data:
+    {country_data}
+
+"""
+    
+    user_choice=input("please choose one, VACATION OR RELOCATION:").strip()
+
+    if user_choice.lower() == "":
+        print("no choice made cannot proceed. please make a choice.")
+        return
+    elif user_choice.lower() == "vacation":
+        response = client.models.generate_content(
+            model="gemini-3.5-flash", 
+            contents=F"{prompt1}"
+        )
+        print(response.text)
+    elif user_choice.lower() == "relocation":
+        response = client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=F"{prompt2}"
+    )
+        print(response.text)
+        
     
 
-
-
-
 country_search()
+
+
