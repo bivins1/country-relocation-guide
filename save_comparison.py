5d17b13044ad5dc8026ead035ad21daf39530521
bivins1 <bigviscious33@gmail.com>
2026-06-23 12:33:24 +0100

    implemented discarded save functions into web app and modified app.py

--- save_comparison.py
+++ save_comparison.py
@@ -1,10 +1,19 @@
 from pathlib import Path
 
-def save_comparison(content: str, filename: str):
+
+def save_comparison(
+    content: str,
+    country1_name: str,
+    country2_name: str
+):
     folder = Path("saved_comparisons")
     folder.mkdir(exist_ok=True)
+
+    filename = (
+        f"{country1_name}_vs_{country2_name}.txt"
+    )
 
     file_path = folder / filename
     file_path.write_text(content, encoding="utf-8")
 
-    print("Comparison saved successfully.")
+    return file_path
--- save_guide.py
+++ save_guide.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
-def save_guide(content: str, filename: str) :
+
+def save_guide(content: str, country_name: str, guide_type: str):
     folder = Path("saved_guides")
     folder.mkdir(exist_ok=True)
 
+    filename = f"{country_name}_{guide_type}_guide.txt"
+
     file_path = folder / filename
     file_path.write_text(content, encoding="utf-8")
-    print("Guide saved successfully")
-    return 
+
+    return file_path
